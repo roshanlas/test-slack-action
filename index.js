1b998fc2eb28648f9a55975c4793fff8f680b183
Roshan Lasrado <roshanlasrado94@gmail.com>
2020-06-14 22:26:00 -0700

    Add Slack capability

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const { App } = require("@slack/bolt");
+
+// Post a message to a channel your app is in using ID and message text
+async function publishMessage(id, text, token) {
+    try {
+      // Call the chat.postMessage method using the built-in WebClient
+      const result = await app.client.chat.postMessage({
+        // The token you used to initialize your app
+        token: token,
+        channel: id,
+        text: text
+        // You could also use a blocks[] array to send richer content
+      });
+  
+      // Print result, which includes information about the message (like TS)
+      console.log(result);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const slackToken = core.getInput('slack-token');
+  const slackSecret = core.getInput('slack-secret');
+  const slackChannelId = core.getInput('slack-channel-id');
+
+  const commitMessage = github.context.payload.commits[0].message;
+
+
+  const app = new App({
+    token: slackToken,
+    signingSecret: slackSecret,
+  });
+
+  (async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+  
+    console.log("⚡️ Bolt app is running!");
+    
+    // After the app starts, publish message
+    publishMessage(
+        slackChannelId, 
+        `Received a ${github.context.eventName} event with the following commit message:\n
+        ${commitMessage}.
+        \n\nThis discussion was classified as: ${["Design Discussion", "Non-Design Discussion"][Math.floor(Math.random() * 2)]}`,
+        slackToken
+    );
+  })();
+
+
+//   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
